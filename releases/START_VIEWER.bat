88a9f67935a76e3379a329f8c005539d0a1d0551
slatinsky <slatinsky.adam@gmail.com>
2022-11-04 18:13:45 +0100

    fix race condition, edge browser fallback

--- releases/START_VIEWER.bat
+++ releases/START_VIEWER.bat
@@ -1,5 +1,15 @@
 pushd "%~dp0"
 cd bin
 preprocess.exe
-if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" ("C:\Program Files\Google\Chrome\Application\chrome.exe" --app="http://127.0.0.1:21011/index.html" --enable-precise-memory-info) else (rundll32 url.dll,FileProtocolHandler http://127.0.0.1:21011/index.html)
-http-server.exe "../static" --port 21011 -c-1 --silent -P http://127.0.0.1:21011?
+start "DiscordChatExporter-staticServer" http-server.exe "../static" --port 21011 -c-1 --silent -P http://127.0.0.1:21011?
+timeout /t 1 /nobreak >nul
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
+    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --app="http://127.0.0.1:21011/index.html" --enable-precise-memory-info
+) else (
+    if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" (
+        start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --app="http://127.0.0.1:21011/index.html" --enable-precise-memory-info
+    ) else (
+        start "" rundll32 url.dll,FileProtocolHandler http://127.0.0.1:21011/index.html
+    )
+)
+cd ..
