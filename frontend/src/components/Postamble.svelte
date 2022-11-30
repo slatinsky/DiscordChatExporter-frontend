7d02840b82c56d12224ac30f20acb859cf9c1046
slatinsky <slatinsky.adam@gmail.com>
2022-11-30 12:19:29 +0100

    delete unused files

--- frontend/src/components/Postamble.svelte
+++
@@ -1,7 +0,0 @@
-<!--<script>-->
-<!--    export let messageCount = 0;-->
-<!--</script>-->
-
-<!--<div class=postamble>-->
-<!--    <div class=postamble__entry>Exported {messageCount} message(s)</div>-->
-<!--</div>-->
