810338f9569341a73c03de8cfa8dd607d511c72e
slatinsky <slatinsky.adam@gmail.com>
2023-01-30 02:06:26 +0100

    wip - attempt to create variable height virtual list

--- frontend/src/components/Scroller.svelte
+++ frontend/src/components/Scroller.svelte
@@ -1,5 +1,6 @@
-<!-- Unfinished virtual scroller component -->
-<!-- Now renders with fixed item heights -->
+<!-- Virtual scroller component -->
+<!-- Now renders with variable item heights -->
+<!-- needs refactor to be more readable :) -->
 
 <script lang="ts">
 	import type { Message } from "src/js/interfaces";
@@ -30,22 +31,28 @@
 	let scrollWindowHeight: number;
 	let scrollWindowWidth: number;
 	let scrollContainerHeightOffset: number;
+	let scrollContainerHeight = estimatedItemHeight * messages.length
 	let scrollFromTop: number;
 
-	let elementsPadding = 2;
+	let elementsPadding = 20;
+	let baseIndex = 0;
+	let baseIndexOffset = 0;
 
 	$: visibleElementsCount = Math.ceil(scrollWindowHeight / estimatedItemHeight);
-	$: topVisibleElementIndex = Math.max(Math.floor(scrollFromTop / estimatedItemHeight) - elementsPadding, 0);
+
+
+	$: baseOffset = (baseIndex * estimatedItemHeight - baseIndexOffset);
+
+	$: topVisibleElementIndex = Math.max(Math.floor((scrollFromTop+baseOffset) / estimatedItemHeight) - elementsPadding, 0);
 	$: bottomVisibleElementIndex = topVisibleElementIndex + visibleElementsCount + elementsPadding * 2;
 	$: renderedMessages = messages.slice(topVisibleElementIndex, bottomVisibleElementIndex);
-
-
 
 
 	$: console.log("scrollWindowHeight, scrollWindowWidth", scrollWindowHeight, scrollWindowWidth)
 	$: console.log("scrollContainerHeightOffset", scrollContainerHeightOffset)
 	$: console.log("visibleElementsCount", visibleElementsCount)
-	$: console.log("scrollFromTop", scrollFromTop)
+	// $: console.log("scrollFromTop", scrollFromTop)
+	$: console.log("baseOffset", baseOffset)
 	$: console.log("topVisibleElementIndex, bottomVisibleElementIndex", topVisibleElementIndex, bottomVisibleElementIndex)
 
 	let domScrollWindow: HTMLElement;
@@ -57,32 +64,151 @@
 		scrollFromTop = domScrollWindow.scrollTop;
 	}
 
+	function onWindowResize(newWidth: number, newHeight: number) {
+		heightsCache = {};  // invalidate cache
+	}
+
+	$: onWindowResize(scrollWindowWidth, scrollWindowHeight);
+
+
+
+	let heightsCache: Record<number, number> = {};
+
 	onMount(() => {
 		domScrollWindow.addEventListener("scroll", scrollListener);
+		scrollListener() // call to init without scrolling
+
+		function migrateBaseIndex(newBaseIndex: number) {
+			let newOffset = baseIndexOffset;
+			if (baseIndex < newBaseIndex - 1) {
+				console.log("migrateBaseIndex v1", baseIndex, "-->" ,newBaseIndex);
+				
+				for (let i = baseIndex; i < newBaseIndex; i++) {
+					newOffset += heightsCache[i];
+				}
+			} else if (baseIndex > newBaseIndex + 1) {
+				console.log("migrateBaseIndex v2", baseIndex, "-->" ,newBaseIndex);
+
+				for (let i = baseIndex - 1; i >= newBaseIndex; i--) {
+					newOffset -= heightsCache[i];
+				}
+			}
+			else {
+				return
+			}
+
+			baseIndex = newBaseIndex;
+			baseIndexOffset = newOffset;
+		}
+
+		function fixTopScrollOutOfBounds(offsetToFix: number) {
+			baseIndexOffset -= offsetToFix;
+			domScrollWindow.scrollTop -= offsetToFix;
+			console.log("fixScrollOutOfBounds", offsetToFix);
+		}
 
 		interval = setInterval(() => {
 			// count the heights of scroll-absolute-element and get the difference from estimatedItemHeight
 			let scrollAbsoluteElements = domScrollWindow.querySelectorAll(".scroll-absolute-element");
-			let scrollAbsoluteElementHeights = Array.from(scrollAbsoluteElements).map((element) => element.clientHeight);
-			let totalHeight = scrollAbsoluteElementHeights.reduce((a, b) => a + b, 0);
-			let difference = totalHeight - (estimatedItemHeight * scrollAbsoluteElementHeights.length);
-			let averageHeight = totalHeight / scrollAbsoluteElementHeights.length;
-			console.log("difference", difference, "averageHeight", averageHeight);
-		}, 1000);
+
+			let minTopPosition = Infinity;
+			// let maxBottomPosition = -Infinity;
+
+			let renderedIndexes = []
+			// save the heights of the elements
+			for (let i = 0; i < scrollAbsoluteElements.length; i++) {
+				let domElement = scrollAbsoluteElements[i];
+				let index = parseInt(domElement.getAttribute("data-index") as string);
+				let height = domElement.clientHeight;
+				heightsCache[index] = height;
+				renderedIndexes.push(index);
+
+				// get absolute top position of the element
+				// let topPosition = parseInt(window.getComputedStyle(domElement).top);
+				// minTopPosition = Math.min(minTopPosition, topPosition);
+				// maxBottomPosition = Math.max(maxBottomPosition, topPosition + height);
+			}
+			let renderedIndexMax = Math.max(...renderedIndexes);
+			let renderedIndexMin = Math.min(...renderedIndexes);
+
+			let renderedIndexCenter = Math.floor((renderedIndexMax + renderedIndexMin) / 2);
+
+			// if baseIndex not in renderedIndexes
+			if (!renderedIndexes.includes(baseIndex)) {
+				heightsCache = {};  // invalidate cache
+				// estimate the position of the new baseIndex
+				baseIndex = renderedIndexCenter
+				baseIndexOffset = baseIndex * estimatedItemHeight;
+				console.warn("baseIndex not in renderedIndexes");
+				return
+			}
+
+
+			// update offsets (position top of the element)
+			if (baseIndex < renderedIndexMax) {
+				// direction: down
+
+				// console.log("renderedIndexes", renderedIndexes);
+
+				// calculate the offset from cache
+				let offset = baseIndexOffset
+				let endCorrection = 0;
+				for (let i = baseIndex; i <= renderedIndexMax; i++) {
+					// if the index is rendered, update the offset
+					if (renderedIndexes.includes(i)) {
+						let domElement = domScrollWindow.querySelector(`.scroll-absolute-element[data-index="${i}"]`)
+						domElement.style.top = offset + "px";
+
+						endCorrection = heightsCache[i] - estimatedItemHeight;
+					}
+					offset += heightsCache[i];
+				}
+
+				// fix length of the scroll container
+				scrollContainerHeight = estimatedItemHeight * messages.length + endCorrection
+
+				
+			}
+			if (baseIndex > renderedIndexMin) {
+				// direction: up
+
+				// calculate the offset from cache
+				let offset = baseIndexOffset + heightsCache[baseIndex];
+				for (let i = baseIndex; i >= renderedIndexMin; i--) {
+					offset -= heightsCache[i];
+					// if the index is rendered, update the offset
+					if (renderedIndexes.includes(i)) {
+						let domElement = domScrollWindow.querySelector(`.scroll-absolute-element[data-index="${i}"]`)
+						domElement.style.top = offset + "px";
+						if (offset < 0) {
+							fixTopScrollOutOfBounds(offset);
+						}
+
+						if (i == 0 && offset > 0) {
+							fixTopScrollOutOfBounds(offset);
+						}
+					}
+				}
+			}
+
+			migrateBaseIndex(renderedIndexCenter);
+
+			// console.log("heightsCache", heightsCache);
+			
+
+		}, 50);
 	});
 
 	onDestroy(() => {
 		clearInterval(interval);
 		domScrollWindow.removeEventListener("scroll", scrollListener);
 	});
-
-	
 </script>
 
 <div class="scroll-window" bind:clientHeight={scrollWindowHeight} bind:clientWidth={scrollWindowWidth} bind:this={domScrollWindow}>
-	<div class="scroll-container" style="height: {estimatedItemHeight * messages.length}px;" bind:this={domScrollContainer}>
+	<div class="scroll-container" style="height: {scrollContainerHeight}px;" bind:this={domScrollContainer}>
 		{#each renderedMessages as message, i (message._id)}
-			<div class="scroll-absolute-element" style={"position: absolute; left: 0px; top: " + (estimatedItemHeight * (topVisibleElementIndex + i)) + "px; width: " + scrollWindowWidth + "px;"}>
+			<div class="scroll-absolute-element" data-index={topVisibleElementIndex + i} style={"position: absolute; left: 0px;top:0px; width: " + scrollWindowWidth + "px;"}>
 				<MessageLoader messageId={message._id} selectedGuildId={selectedGuildId} />
 				<!-- <div style={"background-color:" + randomColor() + ";height: " + randomIntInRange(5, 300) + "px;"}></div> -->
 			</div>
