50b2fd9ec1017d2e8a2f977a7660fa19ede15931
slatinsky <slatinsky.adam@gmail.com>
2023-02-01 02:06:23 +0100

    handle missing channel

--- frontend/src/routes/channels/[guildId]/[channelId]/+page.svelte
+++ frontend/src/routes/channels/[guildId]/[channelId]/+page.svelte
@@ -6,6 +6,7 @@
 
 	import Scroller from "src/components/containers/Scroller2.svelte";
 	import MesssageSpoilerHandler from "src/components/messages/MesssageSpoilerHandler.svelte";
+	import Container from "src/components/containers/Container.svelte";
 </script>
 
 <svelte:head>
@@ -15,17 +16,28 @@
 
 
 {#key data.channelId}
-
-<MesssageSpoilerHandler>
-	<Scroller
-		itemCount={data.messages.length}
-		>
-		<div slot="item" let:index>
-			<MessageLoader messageId={data.messages[index]._id} previousMessageId={data.messages[index - 1]?._id} selectedGuildId={data.guildId} />
-		</div>
-	</Scroller>
-</MesssageSpoilerHandler>
-
+	{#if data.messages.length === 0}
+		<Container>
+			<div class="txt">Channel ID {data.channelId} not found</div>
+		</Container>
+	{:else}
+		<MesssageSpoilerHandler>
+			<Scroller
+				itemCount={data.messages.length}
+				>
+				<div slot="item" let:index>
+					<MessageLoader messageId={data.messages[index]._id} previousMessageId={data.messages[index - 1]?._id} selectedGuildId={data.guildId} />
+				</div>
+			</Scroller>
+		</MesssageSpoilerHandler>
+	{/if}
 {/key}
 
 
+<style>
+	.txt {
+		font-size: 32px;
+		padding-top: 20px;
+		padding-left: 20px;
+	}
+</style>
