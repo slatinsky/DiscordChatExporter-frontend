fab1f3281b50b28aab6e79404ce121a1cfd051fe
slatinsky <slatinsky.adam@gmail.com>
2023-02-07 01:31:38 +0100

    fix DEV.bat

--- DEV.bat
+++ DEV.bat
@@ -8,7 +8,7 @@ start "nginx" nginx.exe -c conf/nginx-dev.conf
 cd ../../frontend
 start "sveltekit" npm run dev
 cd ../backend/fastapi
-start "fastapi" py -m uvicorn main:app --reload
-cd ../backend/mongodb
+start "fastapi" py -m uvicorn app:app --host "0.0.0.0" --port 8000 --workers 1 --reload
+cd ../mongodb
 start "mongodb" mongod --dbpath "db/"
 start "browser" rundll32 url.dll,FileProtocolHandler http://localhost:21012/
