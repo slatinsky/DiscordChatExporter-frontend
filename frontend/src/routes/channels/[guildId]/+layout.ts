b3495bf3aef368aa60a00ac8832963efa7b3284a
slatinsky <slatinsky.adam@gmail.com>
2023-02-07 17:36:15 +0100

    cache channels response

--- frontend/src/routes/channels/[guildId]/+layout.ts
+++ frontend/src/routes/channels/[guildId]/+layout.ts
@@ -5,8 +5,9 @@ import type { Channel, Guild } from "src/js/interfaces";
 export const prerender = false;
 export const ssr = false;
 
-let guildIdFetched: string | null = null;
-let json: Channel | null = null;
+
+let channelsCached: Channel[] | null = [];
+let guildIdCached: string | null | undefined = null;
 
 export const load: Load = async({ fetch, params, parent }) => {
 	let selectedGuildId = params.guildId;
@@ -14,16 +15,19 @@ export const load: Load = async({ fetch, params, parent }) => {
 	let guilds: Guild[] = (await parent()).guilds;
 	let guild: Guild | undefined = guilds.find(g => g._id === selectedGuildId);
 
-    let channels
     let guildId = params.guildId
-    if (guildIdFetched === guildId) {  // prevent fetching the same guild multiple times in a row
-        channels = json
-    } else {
+    let channels = channelsCached
+    if (guildIdCached !== guildId) {  // is cache invalid?
         let response = await fetch('/api/channels?guild_id=' + guildId)
         channels = await response.json()
+        channelsCached = channels
+        guildIdCached = guildId
     }
 
-	let channel = channels.find((c: Channel) => c._id === selectedChannelId);
+	let channel
+    if (channels) {
+        channels.find((c: Channel) => c._id === selectedChannelId);
+    }
 
     return {
         guildId: selectedGuildId,
