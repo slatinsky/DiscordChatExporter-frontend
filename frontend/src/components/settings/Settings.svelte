866674539c6bd5acaa527275879dae21612cfbb6
slatinsky <slatinsky.adam@gmail.com>
2023-07-19 00:57:34 +0200

    mobile gui - open menu on exit from settings

--- frontend/src/components/settings/Settings.svelte
+++ frontend/src/components/settings/Settings.svelte
@@ -18,6 +18,12 @@
 		$isMenuHidden = true;
 	}
 
+
+	function closeBtn() {
+		$settingsShown = false
+		$isMenuHidden = false
+	}
+
 </script>
 
 <svelte:head>
@@ -44,7 +50,7 @@
 
 		<div class="settings">
 
-			<div class="close-btn" on:click={()=>$settingsShown = false}>
+			<div class="close-btn" on:click={closeBtn}>
 				<svg role="img" width="18" height="18" viewBox="0 0 24 24"><path fill="currentColor" d="M18.4 4L12 10.4L5.6 4L4 5.6L10.4 12L4 18.4L5.6 20L12 13.6L18.4 20L20 18.4L13.6 12L20 5.6L18.4 4Z"></path></svg>
 			</div>
 
