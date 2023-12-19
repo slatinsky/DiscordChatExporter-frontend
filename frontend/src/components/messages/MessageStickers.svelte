8aa4dfca1dbec9ba42535952f661fbe9a8979a43
slatinsky <slatinsky.adam@gmail.com>
2023-12-20 00:18:32 +0100

    render lottie stickers

--- frontend/src/components/messages/MessageStickers.svelte
+++ frontend/src/components/messages/MessageStickers.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
 	import type {Sticker } from "src/js/interfaces";
 	import ImageGallery from "src/routes/channels/[guildId]/[channelId]/ImageGallery.svelte";
+	import MessageStickerLottie from "./MessageStickerLottie.svelte";
 
 	export let stickers: Sticker[];
 </script>
 
 <div class="chatlog__reactions">
 	{#each stickers as sticker}
-		<div class="chatlog__sticker">
-			<ImageGallery asset={sticker.source} imgclass={"chatlog__sticker-image"} />
-		</div>
+		{#if sticker?.format === "Lottie"}
+			<MessageStickerLottie asset={sticker.source} />
+		{:else}
+			<div class="chatlog__sticker">
+				<ImageGallery asset={sticker.source} imgclass={"chatlog__sticker-image"} />
+			</div>
+		{/if}
 	{/each}
 </div>
 
